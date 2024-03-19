2f9f160129a330f2b6698ec8284bf7fe7294da83
Ryan Saunders <ryan.s.maguire@gmail.com>
2024-03-19 16:09:35 +0000

    created a client.js file with connect function, exported that function to play.js, added initials of client and played with adding movements. set up user input and a function to handle user input. created an input module and exported stdInput. Added WASD movements.

--- input.js
+++ input.js
@@ -1,6 +1,9 @@
 
+// Stores the active TCP connection object.
+let connection;
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -15,6 +18,35 @@ const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write("Move: up");
+  }
+  if (key === 'a') {
+    connection.write("Move: left");
+  }
+  if (key === 's') {
+    connection.write("Move: down");
+  }
+  if (key === 'd') {
+    connection.write("Move: right");
+  }
+
+  
+  // setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 1000);
+
+  //   setInterval(() => {
+  //     conn.write("Move: left");
+  //   }, 1050);
+
+  //   setInterval(() => {
+  //     conn.write("Move: up");
+  //   }, 1100);
+
+  //   conn.on("data", (data) => {
+  //     console.log("Server says: ", data);
+  //   });
 };
 
 module.exports = { setupInput };
